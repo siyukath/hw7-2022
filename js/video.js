67215465ab34205af505dfb09fab2a723c298d36
siyukath <97421035+siyukath@users.noreply.github.com>
2023-03-28 15:12:55 -0400

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -21,7 +21,7 @@ window.addEventListener("load", function() {
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video",video);
 	video.play();
-	updateVolume()
+	//updateVolume()
 })
 
 document.querySelector("#slider").addEventListener("change", function() {
